820a0010a4b3f1680cbc66a574a348a5010b241c
Zach <zdspringer99@gmail.com>
2021-02-18 17:45:49 -0800

    created a button to act as links to other pages

--- src/Components/RouteCards/RouteCard.js
+++ src/Components/RouteCards/RouteCard.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
 function RouteCard() {
-  return;
-  <div></div>;
+  return (
+    <div>
+      <Button variant="outline" size="lg">
+        Button
+      </Button>
+    </div>
+  );
 }
+
+export default RouteCard;
