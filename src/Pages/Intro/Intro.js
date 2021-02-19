a5ad60237d9068c27c49b5227a54a1e3e2553a75
Zach <zdspringer99@gmail.com>
2021-02-19 13:39:36 -0800

    began work on navigation buttons

--- src/Pages/Intro/Intro.js
+++ src/Pages/Intro/Intro.js
@@ -1,5 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import "./Intro.css";
 
 function Intro() {
-  return <div></div>;
+  return (
+    <div>
+      <Link to="/about">
+        <Button variant="outline-warning" size="lg" id="aboutBtn">
+          About
+        </Button>
+      </Link>
+
+      <Link to="/contact">
+        {" "}
+        <Button variant="outline-warning" size="lg" id="contactBtn">
+          Contact
+        </Button>
+      </Link>
+
+      <Link to="/portfolio">
+        {" "}
+        <Button variant="outline-warning" size="lg" id="portfolioBtn">
+          Portfolio
+        </Button>
+      </Link>
+    </div>
+  );
 }
+
+export default Intro;
