e34a29e241fa75794bfaac4d6e09d87c55e56215
Zach <zdspringer99@gmail.com>
2021-02-19 21:01:27 -0800

    put everything within another div to better place the buttons

--- src/App.js
+++ src/App.js
@@ -13,30 +13,32 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div id="introduction">
-        <p className="holo">
-          Rough pomfret lemon shark plownose chimaera southern sandfish kokanee
-          northern sea robin Antarctic cod. Yellow-and-black triplefin gulper
-          South American Lungfish mahi-mahi, butterflyfish glass catfish
-          soapfish ling gray mullet! Warbonnet denticle herring spiny-back cod
-          straptail tailor zebra loach sea lamprey
-        </p>
+      <div className="innerSelf">
+        <div id="introduction">
+          <p className="holo">
+            Rough pomfret lemon shark plownose chimaera southern sandfish
+            kokanee northern sea robin Antarctic cod. Yellow-and-black triplefin
+            gulper South American Lungfish mahi-mahi, butterflyfish glass
+            catfish soapfish ling gray mullet! Warbonnet denticle herring
+            spiny-back cod straptail tailor zebra loach sea lamprey
+          </p>
+        </div>
+        <br />
+        <div className="imgWrapper">
+          <img src={Cog} alt="Cog" id="omniCog" />
+        </div>
+
+        <Router>
+          <Route exact path="/" component={Intro} />
+
+          <Route exact path="/about" component={About} />
+
+          <Route exact path="/contact" component={Contact} />
+
+          <Route exact path="/portfolio" component={Portfolio} />
+        </Router>
+        <div id="projecter" />
       </div>
-      <br />
-      <div className="imgWrapper">
-        <img src={Cog} alt="Cog" id="omniCog" />
-      </div>
-
-      <Router>
-        <Route exact path="/" component={Intro} />
-
-        <Route exact path="/about" component={About} />
-
-        <Route exact path="/contact" component={Contact} />
-
-        <Route exact path="/portfolio" component={Portfolio} />
-      </Router>
-      <div id="projecter" />
     </div>
   );
 }
