6647e04109f4a85acbc260c47a91c8dd8e963fba
Zach <zdspringer99@gmail.com>
2021-02-21 21:35:40 -0800

    moved all front page related elements to the correct file and out of App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./Pages/About/About";
 import Contact from "./Pages/Contact/Contact";
 import Portfolio from "./Pages/Portfolio/Portfolio";
 import Intro from "./Pages/Intro/Intro";
-import Cog from "./cog_mechanicus_v2.png";
 import "./App.css";
 
 //===================================================================================================================
@@ -13,22 +12,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="innerSelf">
-        <div id="introduction">
-          <p className="holo">
-            Rough pomfret lemon shark plownose chimaera southern sandfish
-            kokanee northern sea robin Antarctic cod. Yellow-and-black triplefin
-            gulper South American Lungfish mahi-mahi, butterflyfish glass
-            catfish soapfish ling gray mullet! Warbonnet denticle herring
-            spiny-back cod straptail tailor zebra loach sea lamprey
-          </p>
-        </div>
-        <br />
-        <div className="imgWrapper">
-          <img src={Cog} alt="Cog" id="omniCog" />
-        </div>
-
-        <Router>
+      <Router>
+        <Switch>
           <Route exact path="/" component={Intro} />
 
           <Route exact path="/about" component={About} />
@@ -36,8 +21,10 @@ function App() {
           <Route exact path="/contact" component={Contact} />
 
           <Route exact path="/portfolio" component={Portfolio} />
-        </Router>
-      </div>
+        </Switch>
+      </Router>
+      <canvas id="noise" className="noise"></canvas>
+      <div className="vignette"></div>
     </div>
   );
 }
