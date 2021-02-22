65540728cd9b29cd1fca387b56ee9a0023b0a039
Zach <zdspringer99@gmail.com>
2021-02-21 21:45:00 -0800

    moved all front page related elements to Intro and linked in the cog

--- src/Pages/Intro/Intro.js
+++ src/Pages/Intro/Intro.js
@@ -1,11 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
+import Cog from "../../cog_mechanicus_v2.png";
+
 import "./Intro.css";
 
 function Intro() {
   return (
-    <div>
+    <div className="Intro">
+      <div className="innerSelf">
+        <div id="introduction">
+          <p className="holo">
+            Rough pomfret lemon shark plownose chimaera southern sandfish
+            kokanee northern sea robin Antarctic cod. Yellow-and-black triplefin
+            gulper South American Lungfish mahi-mahi, butterflyfish glass
+            catfish soapfish ling gray mullet! Warbonnet denticle herring
+            spiny-back cod straptail tailor zebra loach sea lamprey
+          </p>
+        </div>
+        <br />
+        <div className="imgWrapper">
+          <img src={Cog} alt="Cog" id="omniCog" />
+        </div>
+      </div>
       <Link to="/about">
         <Button variant="outline" className="btn-warning" id="aboutBtn">
           About
@@ -13,14 +30,12 @@ function Intro() {
       </Link>
 
       <Link to="/contact">
-        {" "}
         <Button variant="outline" className="btn-warning" id="contactBtn">
           Contact
         </Button>
       </Link>
 
       <Link to="/portfolio">
-        {" "}
         <Button variant="outline" className="btn-warning" id="portfolioBtn">
           Portfolio
         </Button>
