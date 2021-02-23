a93dbb8f8be707ebcd443bc6f5874fc66447c495
Zach <zdspringer99@gmail.com>
2021-02-22 18:39:44 -0800

    added styling and began arranging correctly

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -3,45 +3,73 @@ import BMManager from "./assets/Capture.PNG";
 import PWGenerator from "./assets/immlink1.png";
 import DailyScheduler from "./assets/immlink3.png";
 import WhatToDo from "./assets/immlink2.png";
+import Budget from "./assets/budget_ting.png";
+import Employee_Sorter from "./assets/employee_sorter.png";
+import "./Portfolio.css";
 
 function Portfolio() {
   return (
-    <div className="row">
-      <div className="col-md-6">
-        <a href="https://github.com/Z1springer/password_creation">
-          <img
-            src={PWGenerator}
-            className="img-fluid"
-            alt="password_generator"
-          />
-        </a>
-      </div>
-      <div className="col-md-6">
-        <a href="https://github.com/Z1springer/what_to_do">
-          <img
-            src={WhatToDo}
-            className="img-fluid"
-            alt="first_project_what_to_do"
-          />
-        </a>
-      </div>
-      <div className="col-md-6">
-        <a href="https://github.com/Z1springer/daily_scheduler">
-          <img
-            src={DailyScheduler}
-            className="img-fluid"
-            alt="daily_scheduler"
-          />
-        </a>
-      </div>
-      <div className="col-md-6">
-        <a href="https://github.com/Kray93/Bookmark-Manager">
-          <img
-            src={BMManager}
-            className="img-fluid"
-            alt="second_project_Bookmark-Manager"
-          />
-        </a>
+    <div id="imgHome">
+      <h1>Examples of My Work! Click for GitHub Links!</h1>
+      <div className="row">
+        <div className="col-md-6" className="img">
+          <a href="https://github.com/Z1springer/password_creation">
+            <img
+              src={PWGenerator}
+              className="img-fluid"
+              alt="One of my better assignments"
+            />
+          </a>
+        </div>
+        <div className="col-md-6" className="img">
+          <a href="https://github.com/Z1springer/what_to_do">
+            <img
+              src={WhatToDo}
+              className="img-fluid"
+              alt="My First Project at BC"
+            />
+          </a>
+        </div>
+        <div className="row">
+          <div className="col-md-6" className="img">
+            <a href="https://github.com/Z1springer/daily_scheduler">
+              <img
+                src={DailyScheduler}
+                className="img-fluid"
+                alt="One I put a lot of work in to"
+              />
+            </a>
+          </div>
+          <div className="col-md-6" className="img">
+            <a href="https://github.com/Kray93/Bookmark-Manager">
+              <img
+                src={BMManager}
+                className="img-fluid"
+                alt="My Second Project at BC"
+              />
+            </a>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6" className="img">
+            <a href="https://github.com/Z1springer/sorter-of-employees">
+              <img
+                src={Employee_Sorter}
+                className="img-fluid"
+                alt="One that I was able to do without pair-coding"
+              />
+            </a>
+          </div>
+          <div className="col-md-6" className="img">
+            <a href="https://github.com/Z1springer/budget-cuts">
+              <img
+                src={Budget}
+                className="img-fluid"
+                alt="One of my better assignments"
+              />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
