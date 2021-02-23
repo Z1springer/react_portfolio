ac0e11aa99a6a5b660e687d9f72e4129e5694bd6
Zach <zdspringer99@gmail.com>
2021-02-22 18:37:34 -0800

    added my information and words of caution

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -6,32 +6,54 @@ const Contact = () => {
   return (
     <Container id="contact">
       <Row className="rules">
-        <p id="rules">
-          rules and conduct to follow should someone find the need to contact
-          me, along with what they might expect depending on the time of day
-          they reach out
+        <h2>Pay Heed Those Who Enter Here!</h2>
+        <p>
+          I am not a very fast learner when it comes to a fluid plan. I work far
+          better with a structure and a plan of action. Thinking on my feet is
+          not something that I excel at. There are also going to be moments
+          where I will not understand what the purpose of a situation is, and I
+          assure you it is not due to me not trying to understand, this is an
+          aspect of my aspergers. I beg you, be patient and understand that I am
+          willing to give everything and open up to those who prove they are not
+          out for my head, life has taught me to close myself off, do not take
+          it as me not wishing to speak, just that I do not know what to say.
         </p>
       </Row>
       <Row className="contacts">
         <Col xs="4" className="links">
-          <h4>Links!</h4>
+          <h3>Links!</h3>
           <ul>
-            <li>Github:</li>
-            <li>LinkedIn:</li>
+            <li>
+              <a href="https://github.com/Z1springer">My GitHub Account</a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/zach-springer-77b434200/">
+                My LinkedIn Account
+              </a>
+            </li>
             <li>No Social Media Links worth Mentioning</li>
           </ul>
         </Col>
         <Col xs="4" className="emailnphone">
-          <p>my email and phone number will go here</p>
+          <h3>My Info</h3>
+          <p>Email: zdspringer99@gmail.com</p>
+          <p>Phone Number: 253-592-9629</p>
         </Col>
         <Col xs="4" className="resume">
-          <p>A link and image of a resume will go here</p>
+          <h3>My Resume</h3>
+          <p>!!A link and image of a resume will go here!!</p>
         </Col>
       </Row>
       <Row xs="8" className="aword">
+        <h2>One Last Thing...</h2>
         <p>
-          a short warning for those who wish to shirk the rules of conduct when
-          in these parts and just what my imediate reaction shall be
+          I am not a fan of speaking with automated systems, nor am I very good
+          at being spoken to through a script, as such; I will hang up any call
+          that I believe to be in any way automated.
+          <br />
+          <br />
+          Furthermore, I will delete any emails that reacure over and over, I
+          have enough filling my inbox
         </p>
       </Row>
     </Container>
